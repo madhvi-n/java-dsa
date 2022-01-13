cb5ec6f0551a8511db8a7677fdab701249b0bf7e
Madhvi Ń <madhvinandanwar@gmail.com>
2022-01-13 16:03:48 +0530

    Assignments

--- code/src/assignments/Fibonacci.java
+++ code/src/assignments/Fibonacci.java
@@ -1,15 +1,31 @@
 package assignments;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Fibonacci {
     public static void main(String[] args) {
-        int num = 10;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Find fibonacci series with n elements: ");
+        int num = in.nextInt();
+
+        String series = fibonacci(num);
+        System.out.println(series);
+    }
+
+    static String fibonacci(int range) {
         int first = 0;
         int second = 1;
-        for (int i = 1; i <= num; ++i) {
-            System.out.print(first + ", ");
+        int[] res = new int[range];
+
+        for (int i = 1; i < range; i++) {
             int next = first + second;
+            res[i] = next;
             first = second;
             second = next;
         }
+
+        return Arrays.toString(res);
     }
 }
