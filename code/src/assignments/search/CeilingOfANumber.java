290ed866b3268bb3404436b4d7a4f57330cabb05
Madhvi Ń <madhvinandanwar@gmail.com>
2022-01-17 13:33:10 +0530

    Ceiling of a number

---
+++ code/src/assignments/search/CeilingOfANumber.java
@@ -0,0 +1,73 @@
+package assignments.search;
+
+public class CeilingOfANumber {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 9, 14, 15, 18, 22};
+        int[] arr2 = {22, 18, 15, 14, 9, 5, 3, 2};
+
+        // Number
+        System.out.println(search(arr, 14));
+        System.out.println(search(arr, 16));
+
+        //Indexes
+        System.out.println(ceil(arr, 9));
+        System.out.println(ceil(arr, 17));
+
+        // Number
+        System.out.println(search(arr2, 9));
+        System.out.println(search(arr2, 17));
+    }
+
+    // Returns index of ceil of a target
+    static int ceil(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while( start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(target > arr[mid]) {
+                start = mid +1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+
+    // Returns ceil of a target irrespective of sorting order
+    static int search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        boolean isAsc = arr[start] < arr[end];
+
+        while( start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(target == arr[mid]) {
+                return arr[mid];
+            }
+
+            if(isAsc) {
+                if(target > arr[mid]) {
+                    start = mid +1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                if(target < arr[mid]) {
+                    start = mid +1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        if(isAsc){
+            return arr[start];
+        }
+        return arr[end];
+    }
+
+}
