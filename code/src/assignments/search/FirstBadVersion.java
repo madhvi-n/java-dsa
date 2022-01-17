04147e50f903ed417e7061de643ed089a20a024b
Madhvi Ń <madhvinandanwar@gmail.com>
2022-01-17 19:55:13 +0530

    First bad version

---
+++ code/src/assignments/search/FirstBadVersion.java
@@ -0,0 +1,28 @@
+package assignments.search;
+
+public class FirstBadVersion {
+    public static void main(String[] args) {
+        System.out.println(firstBadVersion(1));
+    }
+
+    static int firstBadVersion(int n) {
+        int start = 0;
+        int end = n;
+
+        while(start < end) {
+            mid =  start + (end - start) / 2;
+            check = isBadVersion(n);
+            if(check) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+    // Dummy code for isBadVersion api of leetcode
+    static boolean isBadVersion(int n) {
+        return true;
+    }
+}
