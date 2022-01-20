cad78fc38da90f4291bb835ff479ddd7e22b100d
MayaScarlet <madhvinandanwar@gmail.com>
2022-01-20 11:34:50 +0530

    Selection sort

---
+++ code/src/sorting_algorithms/SelectionSort.java
@@ -0,0 +1,37 @@
+package sorting_algorithms;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {0, 4, 5, 2, 6, 8, 3, 1};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // Find the max in the remaining array and swap it with correct index
+
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    static int getMaxIndex(int[] arr, int startIndex, int endIndex) {
+        int max = startIndex;
+        for (int i = startIndex; i <= endIndex; i++) {
+            if(arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
