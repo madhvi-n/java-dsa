5885a31deb17f4fb5263975057e5c9bfba23c994
MayaScarlet <madhvinandanwar@gmail.com>
2022-01-20 15:44:08 +0530

    Cyclic sort

---
+++ code/src/sorting_algorithms/CyclicSort.java
@@ -0,0 +1,29 @@
+package sorting_algorithms;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 1, 5, 7, 3, 8, 6};
+        cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void cyclicSort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+            if(arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
